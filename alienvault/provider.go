d80a51aba14bb08e8eecbdadc9357dca85d98545
Liam Galvin <liam@liam-galvin.co.uk>
2019-05-23 11:08:13 +0100

    Add skip- TLS verification option

--- alienvault/provider.go
+++ alienvault/provider.go
@@ -1,40 +1,47 @@
 package alienvault
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+    "github.com/hashicorp/terraform/helper/schema"
 )
 
 // Provider makes the AlienVault provider available
 func Provider() *schema.Provider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"fqdn": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The fully qualified domain name for your AlienVault instance e.g. example.alienvault.cloud",
-				DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_FQDN", nil),
-				Sensitive:   true,
-			},
-			"username": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "AV username",
-				DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_USERNAME", nil),
-				Sensitive:   true,
-			},
-			"password": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "AV password",
-				DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_PASSWORD", nil),
-				Sensitive:   true,
-			},
-		},
-		ResourcesMap: map[string]*schema.Resource{
-			"alienvault_job_aws_bucket":     resourceJobAWSBucket(),
-			"alienvault_job_aws_cloudwatch": resourceJobAWSCloudWatch(),
-			"alienvault_sensor":             resourceSensor(),
-		},
-		ConfigureFunc: providerConfigure,
-	}
+    return &schema.Provider{
+        Schema: map[string]*schema.Schema{
+            "fqdn": &schema.Schema{
+                Type:        schema.TypeString,
+                Required:    true,
+                Description: "The fully qualified domain name for your AlienVault instance e.g. example.alienvault.cloud",
+                DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_FQDN", nil),
+                Sensitive:   true,
+            },
+            "username": &schema.Schema{
+                Type:        schema.TypeString,
+                Required:    true,
+                Description: "AV username",
+                DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_USERNAME", nil),
+                Sensitive:   true,
+            },
+            "password": &schema.Schema{
+                Type:        schema.TypeString,
+                Required:    true,
+                Description: "AV password",
+                DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_PASSWORD", nil),
+                Sensitive:   true,
+            },
+            "skip_tls_verify ": &schema.Schema{
+                Type:        schema.TypeBool,
+                Optional:    true,
+                Description: "Skip TLS certificate verification",
+                DefaultFunc: schema.EnvDefaultFunc("ALIENVAULT_SKIP_TLS_VERIFY", nil),
+                Default:     false,
+            },
+        },
+        ResourcesMap: map[string]*schema.Resource{
+            "alienvault_job_aws_bucket":     resourceJobAWSBucket(),
+            "alienvault_job_aws_cloudwatch": resourceJobAWSCloudWatch(),
+            "alienvault_sensor":             resourceSensor(),
+        },
+        ConfigureFunc: providerConfigure,
+    }
 }
