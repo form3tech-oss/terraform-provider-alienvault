332fa696a47c2df8239439bedda04a27bac6cc8b
Liam Galvin <liam@liam-galvin.co.uk>
2019-05-23 11:19:49 +0100

    Fix skip tls value

--- alienvault/configure.go
+++ alienvault/configure.go
@@ -7,13 +7,19 @@ import (
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 
+    var skipTLS bool
+    skipTLSRaw, ok := d.GetOk("skip_tls_verify")
+    if ok {
+        skipTLS = skipTLSRaw.(bool)
+    }
+
     client := alienvault.New(
         d.Get("fqdn").(string),
         alienvault.Credentials{
             Username: d.Get("username").(string),
             Password: d.Get("password").(string),
         },
-        d.Get("skip_tls_verify").(bool),
+        skipTLS,
     )
 
     if err := client.Authenticate(); err != nil {
