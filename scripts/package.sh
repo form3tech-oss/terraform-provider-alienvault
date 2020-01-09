b2b1de7b7da70c9927bbac8e0dc7b049e26a424b
Liam Galvin <liam@liam-galvin.co.uk>
2020-01-09 14:51:38 +0000

    Update package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
+BINARY="terraform-provider-alienvault_${TRAVIS_TAG}"
+GO111MODULE=on
 
-set -x
+GOOS=darwin GOARCH=amd64 go build -o "${BINARY}"
+zip "${BINARY}_darwin_amd64.zip" "${BINARY}"
+rm -f "${BINARY}"
 
-BINARY="terraform-provider-alienvault_${TRAVIS_TAG}"	BINARY="terraform-provider-alienvault_${TRAVIS_TAG}"
-GO111MODULE=on	GO111MODULE=on
+GOOS=linux GOARCH=amd64 go build -o "${BINARY}"
+zip "${BINARY}_linux_amd64.zip" "${BINARY}"
+rm -f "${BINARY}"
 
-
-GOOS=darwin GOARCH=amd64 go build -o "${BINARY}"	package () {
-zip "${BINARY}_darwin_amd64.zip" "${BINARY}"	  GOOS=$1
-rm -f "${BINARY}"	  echo $GOOS
-
-  dir="${BINARY}_${GOOS}_amd64"
-GOOS=linux GOARCH=amd64 go build -o "${BINARY}"	  mkdir "${dir}"
-zip "${BINARY}_linux_amd64.zip" "${BINARY}"	  GOOS=$1 GOARCH=amd64 go build -o "${dir}/${BINARY}"
-rm -f "${BINARY}"	  zip "${BINARY}_${GOOS}_amd64.zip" -r "${dir}"
-  rm -rf "./${dir:?}/"
-}
-
-
-GOOS=windows GOARCH=amd64 go build -o "${BINARY}"	package darwin
-zip "${BINARY}_windows_amd64.zip" "${BINARY}"	package linux
+GOOS=windows GOARCH=amd64 go build -o "${BINARY}"
+zip "${BINARY}_windows_amd64.zip" "${BINARY}"
 rm -f "${BINARY}"
