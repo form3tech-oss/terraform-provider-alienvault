fb64eec7d23db4c342cd9c877ce059516afc0c5e
Matej Baco <matej.baco@form3.tech>
2020-05-07 16:40:10 +0200

    Add more auth error context

--- alienvault/configure.go
+++ alienvault/configure.go
@@ -1,26 +1,26 @@
 package alienvault
 
 import (
-    "github.com/form3tech-oss/alienvault"
-    "github.com/hashicorp/terraform/helper/schema"
+	"fmt"
+
+	"github.com/form3tech-oss/alienvault"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	client := alienvault.New(
+		d.Get("fqdn").(string),
+		alienvault.Credentials{
+			Username: d.Get("username").(string),
+			Password: d.Get("password").(string),
+		},
+		d.Get("skip_tls_verify").(bool),
+		d.Get("api_version").(int),
+	)
 
-    client := alienvault.New(
-        d.Get("fqdn").(string),
-        alienvault.Credentials{
-            Username: d.Get("username").(string),
-            Password: d.Get("password").(string),
-        },
-        d.Get("skip_tls_verify").(bool),
-        d.Get("api_version").(int),
+	if err := client.Authenticate(); err != nil {
+		return nil, fmt.Errorf("failed in authenticate: %w", err)
+	}
 
-    )
-
-    if err := client.Authenticate(); err != nil {
-        return nil, err
-    }
-
-    return client, nil
+	return client, nil
 }
