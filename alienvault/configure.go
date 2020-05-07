1839cddda8ec2eab33119ad84993ca5da07c32ae
Matej Baco <matej.baco@form3.tech>
2020-05-07 16:40:10 +0200

    Add more auth error context

--- alienvault/configure.go
+++ alienvault/configure.go
@@ -1,26 +1,35 @@
 package alienvault
 
 import (
-    "github.com/form3tech-oss/alienvault"
-    "github.com/hashicorp/terraform/helper/schema"
+	"fmt"
+
+	"github.com/form3tech-oss/alienvault"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	client := alienvault.New(
+		d.Get("fqdn").(string),
+		alienvault.Credentials{
+			Username: d.Get("username").(string),
+			Password: d.Get("password").(string),
+		},
+		d.Get("skip_tls_verify").(bool),
+		d.Get("api_version").(int),
+	)
 
-    client := alienvault.New(
-        d.Get("fqdn").(string),
-        alienvault.Credentials{
-            Username: d.Get("username").(string),
-            Password: d.Get("password").(string),
-        },
-        d.Get("skip_tls_verify").(bool),
-        d.Get("api_version").(int),
+	if err := client.Authenticate(); err != nil {
+		return nil, fmt.Errorf("failed in authenticate (u: %s, p: %s): %w", d.Get("username").(hiddenValue), d.Get("password").(hiddenValue), err)
+	}
 
-    )
+	return client, nil
+}
 
-    if err := client.Authenticate(); err != nil {
-        return nil, err
-    }
+type hiddenValue interface{}
 
-    return client, nil
+func (v hiddenValue) String() string {
+	if len(v) < 3 {
+		return "****"
+	}
+	return "****" + string(v[len(v)-3:])
 }
